fb162031d3dc52ec7e509772d5dc6ed28d5f81ab
Henry Marin <henrymarin@rocketmail.com>
2020-03-31 10:05:48 -0500

    added Footer

---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,13 @@
+import React, {Component} from 'react';
+
+class Footer extends Component{
+    render(){
+        return(
+            <footer>
+                <p>Copyright &copy; React Actors</p>
+            </footer>
+        )
+    }
+}
+
+export default Footer;
